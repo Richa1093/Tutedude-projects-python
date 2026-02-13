9c9f7b019bbf9bc41f50a6a6eb8de67ddcf452da
Richa1093 <81007783+Richa1093@users.noreply.github.com>
2026-02-13 20:33:13 +0530

    Add files via upload

---
+++ Task-1(Arthmatic calculation).py
@@ -0,0 +1,11 @@
+print("Lets do some Calculation....")
+num1= int(input("Enter the  first number : "))
+num2= int(input("Enter the second number : "))
+add_result = num1+num2
+print("\n\nAddition: ",add_result)
+sub_result = num1 - num2
+print("SUbtraction: ",sub_result)
+mul_result = num1 * num2
+print("Multiplication: ",mul_result)
+div_result = num1 / num2
+print("Division: ",div_result)
